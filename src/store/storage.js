cf78df9b5752d3795d5639c52dd3c55998061d97
djfksdl <djfksdl@naver.com>
2024-04-16 16:54:54 +0900

    ff

---
+++ src/store/storage.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex';
+export default createStore({
+    state() {
+        return {
+            // apiBaseUrl: "http://localhost:9001",
+            apiBaseUrl: "http://13.125.251.10:9001",
+        };
+    },
+    mutations: {
+
+
+    },
+    plugins: [
+    ]
+});
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir:"C:/javaStudy/workspace_vue_attach/api-attach/src/main/resources/static"
 })
